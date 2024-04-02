67062f9f0cc93d0a99ee76d246ab58aa7bc4b97e
tranhuynhbaokhang <baokhang.tranhuynh@officience.com>
2024-04-02 07:30:26 +0700

    update

--- wp-content/themes/volcano/assets/js/features/service-popup.js
+++ wp-content/themes/volcano/assets/js/features/service-popup.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //run function
     popup_service_detail();
 });
@@ -8,27 +8,38 @@ function popup_service_detail() {
     post_list_element.click(function () {
         let title = $(this).attr('data-title');
         let detail = $(this).attr('data-detail');
-        let thumbnails = ($(this).attr('data-thumbnail')).split(',');
+        let thumbnail = $(this).attr('data-thumbnail');
+
+
         let popup_element = $('.service-post-popup-body');
-        popup_element.children('.service-post-thumbnail').html();
-        $.each(thumbnails,function (index,value) {
-            popup_element.children('.service-post-thumbnail').append('<div class="item-slider-img"><img src="'+value+'" alt="service_post_banner"></div>');
-        });
-		
-		// popup_element.children('.service-post-thumbnail').slick({
-		// 	slidesToShow: 1,
-		// 	slidesToScroll: 1,
-		// 	arrows: false,
-		// 	dots: false,
-		// 	infinite: true,
-		// 	autoplay: false,
-		// 	speed: 600,
-		// 	autoplaySpeed: 5000
-		// });
-		
+        popup_element.children('.service-post-thumbnail').html('');
+        popup_element.children('.service-post-thumbnail').removeClass('slick-initialized');
+        popup_element.children('.service-post-thumbnail').removeClass('slick-slider');
+        let gallery = JSON.parse($(this).attr('data-gallery'));
+        if (gallery) {
+            $.each(gallery, function (index, url) {
+                popup_element.children('.service-post-thumbnail').append('<div class="item-slider-img"><img src="' + url + '" alt="service_post_banner"></div>')
+            })
+        } else {
+            popup_element.children('.service-post-thumbnail').html('<div class="item-slider-img"><img src="' + thumbnail + '" alt="service_post_banner"></div>');
+        }
         popup_element.find('.post-title').html(title);
         popup_element.find('.post-detail').html(detail);
 
+        popup_element.children('.service-post-thumbnail').slick({
+        	slidesToShow: 1,
+        	slidesToScroll: 1,
+        	arrows: true,
+        	dots: false,
+        	infinite: true,
+        	autoplay: false,
+        	speed: 600,
+        	autoplaySpeed: 5000
+        });
+
+        // $(this).closest('.slick-arrow').trigger('click');
+        $('.slick-arrow').trigger('click');
+
         $('.show-service-popup-btn').trigger('click');
-    });
+    })
 }
--- wp-content/themes/volcano/template/service.php
+++ wp-content/themes/volcano/template/service.php
@@ -63,7 +63,7 @@ $id_page = get_queried_object()->ID; ?>
                     </h2>
                 </div>
                 <div class="sub-title-description wow animate__fadeInUp" data-wow-delay="1.1s">
-                    <?= get_field('sub_title_magic_box_service', $id_page);?>
+                    <?= get_field('sub_title_magic_box_service', $id_page); ?>
                 </div>
                 <div class="row row-custom">
                     <?php
@@ -71,7 +71,8 @@ $id_page = get_queried_object()->ID; ?>
                     if ($services) :
                         $totalItemServices = count($services);
                         foreach ($services as $item => $service) : ?>
-                            <div class="col-lg-4 col-md-6 col-12 service-item service-item-<?= $item; ?> wow animate__zoomIn" data-wow-delay="<?= $item/$totalItemServices;?>s">
+                            <div class="col-lg-4 col-md-6 col-12 service-item service-item-<?= $item; ?> wow animate__zoomIn"
+                                 data-wow-delay="<?= $item / $totalItemServices; ?>s">
                                 <div class="cont-box">
                                     <div class="group-img">
                                         <div class="icon"><img src="<?php echo esc_url($service['logo']['url']); ?>"
@@ -93,90 +94,94 @@ $id_page = get_queried_object()->ID; ?>
 
         <?php
         $banner_top_provider = get_field('banner_top_service_provider', $id_page);
-        if($banner_top_provider): ?>
-        <div class="banner-top-provider" style="background-image: url('<?= $banner_top_provider['background_banner']['url']; ?>')">
-            <div class="group-content">
-                <div class="container">
-                    <div class="content">
-                        <div class="icon wow animate__flipInX" data-wow-delay="1s">
-                            <img src="<?= $banner_top_provider['icon_quote']['url'];?>" alt="icon quote">
+        if ($banner_top_provider): ?>
+            <div class="banner-top-provider"
+                 style="background-image: url('<?= $banner_top_provider['background_banner']['url']; ?>')">
+                <div class="group-content">
+                    <div class="container">
+                        <div class="content">
+                            <div class="icon wow animate__flipInX" data-wow-delay="1s">
+                                <img src="<?= $banner_top_provider['icon_quote']['url']; ?>" alt="icon quote">
+                            </div>
+                            <div class="heading wow animate__fadeInLeft"><?= $banner_top_provider['heading']; ?></div>
                         </div>
-                        <div class="heading wow animate__fadeInLeft"><?= $banner_top_provider['heading'];?></div>
                     </div>
                 </div>
             </div>
-        </div>
         <?php endif; ?>
         <!-- Start services section -->
-        <?php if( $service_groups = get_field('services_group', $id_page)):?>
-            <div class="service-list-section" id = "service-list-section">
+        <?php if ($service_groups = get_field('services_group', $id_page)): ?>
+            <div class="service-list-section" id="service-list-section">
                 <div class="container">
                     <?php
-                    $array_align = ['left','center','right'];
+                    $array_align = ['left', 'center', 'right'];
                     $count = 0;
                     $cond = 0;
                     foreach ($service_groups as $group):
                         $count++;
                         $condition = $cond;
-                        if($condition >= 2) {
+                        if ($condition >= 2) {
                             $cond = 0;
                         } else {
-                            $cond ++;
+                            $cond++;
                         }
-                    ?>
-                    <div class="service-item">
-                        <div class="group-heading <?= $array_align[$condition];?> wow animate__fadeInUp">
-                            <h2>
-                                <span class="number"><?php if($count<10){ echo'0';}?><?= $count;?>.</span><?= $group['title'] ?>
-                            </h2>
-                        </div>
-                        <?php if($group['posts']):?>
-                        <div class="post-list">
-                            <?php
-                            foreach ($group['posts'] as $key=> $post): $key++;
-                                $thumbnailImages = $post['thumbnail']['image_thumbnail'];
-                            foreach ($thumbnailImages as $image){
-                                $thumbnailImageUrls[] = $image['url'];
-                            }
-                            ?>
-                            <div data-title="<?= $post['title'] ?>"
-                            data-detail="<?= $post['detail'] ?>"
-                            data-thumbnail="<?= implode(',',$thumbnailImageUrls) ?>"
-                            class="service-post-item wow animate__fadeInUp" data-wow-delay="1s">
-                                <div class="cont-img">
-                                    <img src="<?= $post['thumbnail']['image_thumbnail']['url'] ?>" alt="service-post-thumbnail">
-                                    <?php
-                                        if($post['thumbnail']['movie_text']) {?>
-                                            <div class="bg-movie">
-                                                <div class="group-move-text">
-                                                    <div class="move-text">
-                                                        <div class="item"><?= $post['thumbnail']['movie_text'];?></div>
-                                                    </div>
-                                                    <div class="move-text">
-                                                        <div class="item"><?= $post['thumbnail']['movie_text'];?></div>
-                                                    </div>
-                                                </div>
-                                            </div><?php
-                                        }
-                                    ?>
-                                </div>
-                                <div class="title"><?= $post['title'] ?></div>
-                                <div class="button-volcano">
-                                    <span class="arrow-line">
-                                        <img width="15" src="<?= DF_IMAGE .'/arrow-line-right.png'?>" alt="icon arrow">
-                                    </span>
-                                    <span class="text"><?= __('Read more','volcano') ?></span>
-                                </div>
+                        ?>
+                        <div class="service-item">
+                            <div class="group-heading <?= $array_align[$condition]; ?> wow animate__fadeInUp">
+                                <h2>
+                                    <span class="number"><?php if ($count < 10) {
+                                            echo '0';
+                                        } ?><?= $count; ?>.</span><?= $group['title'] ?>
+                                </h2>
                             </div>
-                            <?php endforeach; ?>
+                            <?php if ($group['posts']): ?>
+                                <div class="post-list">
+                                    <?php
+                                    foreach ($group['posts'] as $key => $post): $key++;
+                                        $gallery = $post['thumbnail']['gallery'];
+                                        $json_data = json_encode($gallery, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+                                        ?>
+                                        <div data-title="<?= $post['title'] ?>"
+                                             data-detail="<?= $post['detail'] ?>"
+                                             data-thumbnail="<?= $post['thumbnail']['image_thumbnail']['url'] ?>"
+                                             data-gallery="<?= htmlspecialchars($json_data, ENT_QUOTES, 'UTF-8'); ?>"
+                                             class="service-post-item wow animate__fadeInUp" data-wow-delay="1s">
+                                            <div class="cont-img">
+                                                <img src="<?= $post['thumbnail']['image_thumbnail']['url'] ?>"
+                                                     alt="service-post-thumbnail">
+                                                <?php
+                                                if ($post['thumbnail']['movie_text']) { ?>
+                                                    <div class="bg-movie">
+                                                    <div class="group-move-text">
+                                                        <div class="move-text">
+                                                            <div class="item"><?= $post['thumbnail']['movie_text']; ?></div>
+                                                        </div>
+                                                        <div class="move-text">
+                                                            <div class="item"><?= $post['thumbnail']['movie_text']; ?></div>
+                                                        </div>
+                                                    </div>
+                                                    </div><?php
+                                                }
+                                                ?>
+                                            </div>
+                                            <div class="title"><?= $post['title'] ?></div>
+                                            <div class="button-volcano">
+                                    <span class="arrow-line">
+                                        <img width="15" src="<?= DF_IMAGE . '/arrow-line-right.png' ?>"
+                                             alt="icon arrow">
+                                    </span>
+                                                <span class="text"><?= __('Read more', 'volcano') ?></span>
+                                            </div>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
+                            <?php endif; ?>
                         </div>
-                        <?php endif;?>
-                    </div>
                     <?php endforeach;
                     ?>
                 </div>
             </div>
-        <?php endif;?>
+        <?php endif; ?>
         <!-- End services section -->
         <div class="summary-information wow animate__fadeInLeft">
             <?php
@@ -217,7 +222,7 @@ $id_page = get_queried_object()->ID; ?>
         <!-- Start service form -->
         <div class="service-form-section">
             <?php
-            $service_form_group = get_field('service_form',$id_page);
+            $service_form_group = get_field('service_form', $id_page);
             $form_title = $service_form_group['form_title'];
             $form_description = $service_form_group['form_description'];
             $form_shortcode = $service_form_group['form_shortcode'];
@@ -240,23 +245,31 @@ $id_page = get_queried_object()->ID; ?>
 
         <!-- Member popup modal -->
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary hidden show-service-popup-btn" data-toggle="modal" data-target="#exampleModalCenter">
+        <button type="button" class="btn btn-primary hidden show-service-popup-btn" data-toggle="modal"
+                data-target="#exampleModalCenter">
             Launch demo modal
         </button>
 
         <!-- Modal -->
-        <div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog"
+             aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="close" data-dismiss="modal" aria-label="Close">
-                        <svg class="svg-icon" aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><polygon fill="" fill-rule="evenodd" points="6.852 7.649 .399 1.195 1.445 .149 7.899 6.602 14.352 .149 15.399 1.195 8.945 7.649 15.399 14.102 14.352 15.149 7.899 8.695 1.445 15.149 .399 14.102"></polygon></svg>
+                        <svg class="svg-icon" aria-hidden="true" role="img" focusable="false"
+                             xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                            <polygon fill="" fill-rule="evenodd"
+                                     points="6.852 7.649 .399 1.195 1.445 .149 7.899 6.602 14.352 .149 15.399 1.195 8.945 7.649 15.399 14.102 14.352 15.149 7.899 8.695 1.445 15.149 .399 14.102"></polygon>
+                        </svg>
                     </div>
                     <div class="modal-body service-post-popup-body row">
                         <div class="col-md-6 col-sm-12 service-post-thumbnail"></div>
                         <div class="col-md-6 col-sm-12 service-post-content">
                             <div class="group-content">
                                 <div class="icon">
-                                    <img height="65" src="<?=  get_field('banner_top_service_provider', $id_page)['icon_quote']['url']?>" alt="icon">
+                                    <img height="65"
+                                         src="<?= get_field('banner_top_service_provider', $id_page)['icon_quote']['url'] ?>"
+                                         alt="icon">
                                 </div>
                                 <div class="post-title"></div>
                                 <div class="post-detail"></div>
